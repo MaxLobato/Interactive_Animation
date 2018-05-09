2786570dbadaf0ffb5e1f62221dac77a74dddcfc
Max W Lobato <ml93117@srcs.loc>
2018-05-09 12:19:54 -0700

    gfshhgf

--- Interactive_Animation.js
+++ Interactive_Animation.js
@@ -6,13 +6,43 @@ src.value = "pictures/weird_guy_staring.png";
 bclass.value = "normal";
 background.setAttributeNode(bclass);
 lordofchaos.setAttributeNode(src);
+lordofchaos.style.height = "2700px";
+lordofchaos.style.width = "2200px";
 
-document.body.onmousedown = function(){
-    bclass.value = "evil";
-}
-document.body.onmouseup = function() {
-    bclass.value = "normal";
-}
-document.body.onkeydown = function() {
 
+function grow() {
+    for (i = 1; i < 100000000; i++) {
+        setInterval(function () {
+            lordofchaos.style.height = (3000 + Math.pow(i, 2)) + 'px';
+            lordofchaos.style.width = (2000 + Math.pow(i, 2)) + 'px';
+        }, 1000 + i);
+
+    }
 }
+
+
+
+    document.body.onmousedown = function () {
+        bclass.value = "evil";
+    }
+    document.body.onmouseup = function () {
+        bclass.value = "normal";
+    }
+    document.body.onkeyup = function () {
+        bclass.value = "normal";
+        src.value = "pictures/weird_guy_staring.png";
+        lordofchaos.style.height = "2700px";
+        lordofchaos.style.width = "2200px";
+    }
+
+    document.body.onkeydown = function (e) {
+        if (e.keyCode === 27) {
+            bclass.value = "youcant";
+        }
+        if (e.keyCode === 67) {
+            bclass.value = "wheezer";
+            src.value = "pictures/6322.png";
+            grow();
+
+        }
+    }
