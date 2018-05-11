d1f37d0d7bb4b67e0013606d78eeac967fd117e9
Max W Lobato <ml93117@srcs.loc>
2018-05-11 10:53:54 -0700

    reee

--- Interactive_Animation.js
+++ Interactive_Animation.js
@@ -1,38 +1,46 @@
 var lordofchaos = document.getElementById('lordofchaos');
 var background = document.getElementById('background');
+var cursor = document.getElementById('mouse');
 var src = document.createAttribute('src');
+var src2 = document.createAttribute('src');
 var bclass = document.createAttribute('class');
 src.value = "pictures/weird_guy_staring.png";
+src2.value = "pictures/guy_staring.PNG";
 bclass.value = "normal";
 background.setAttributeNode(bclass);
 lordofchaos.setAttributeNode(src);
+cursor.setAttributeNode(src2);
 lordofchaos.style.height = "2700px";
 lordofchaos.style.width = "2200px";
 
 
-function grow() {
-    for (i = 1; i < 100000000; i++) {
-        setInterval(function () {
-            lordofchaos.style.height = (3000 + Math.pow(i, 2)) + 'px';
-            lordofchaos.style.width = (2000 + Math.pow(i, 2)) + 'px';
-        }, 1000 + i);
 
-    }
+function update(event){
+    var x = event.clientX;
+    var y = event.clientY;
+
+    cursor.style.top = y + 'px';
+    cursor.style.left = x + 'px';
 }
+
+document.addEventListener("mousemove", update);
 
 
 
     document.body.onmousedown = function () {
         bclass.value = "evil";
+        src2.value = "pictures/weird_guy_staring_evil.png";
     }
     document.body.onmouseup = function () {
         bclass.value = "normal";
+         src2.value = "pictures/guy_staring.PNG";
     }
     document.body.onkeyup = function () {
         bclass.value = "normal";
         src.value = "pictures/weird_guy_staring.png";
         lordofchaos.style.height = "2700px";
         lordofchaos.style.width = "2200px";
+        src2.value = "pictures/guy_staring.png";
     }
 
     document.body.onkeydown = function (e) {
@@ -42,7 +50,10 @@ function grow() {
         if (e.keyCode === 67) {
             bclass.value = "wheezer";
             src.value = "pictures/6322.png";
-            grow();
+            src2.value = "pictures/images.jpg";
 
+        }
+        if (e.keyCode === 68) {
+            bclass.value = "me";
         }
     }
