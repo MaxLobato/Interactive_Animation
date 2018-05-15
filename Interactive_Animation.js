a6fa70208b95cd5bb652aad760979063ae93f366
Max W Lobato <ml93117@srcs.loc>
2018-05-15 11:45:48 -0700

    i need ehlp

--- Interactive_Animation.js
+++ Interactive_Animation.js
@@ -1,23 +1,27 @@
 var lordofchaos = document.getElementById('lordofchaos');
 var background = document.getElementById('background');
+
 var cursor = document.getElementById('mouse');
 var src = document.createAttribute('src');
 var src2 = document.createAttribute('src');
+var srcboard = document.createAttribute('src');
 var bclass = document.createAttribute('class');
 src.value = "pictures/weird_guy_staring.png";
 src2.value = "pictures/guy_staring.PNG";
+
 bclass.value = "normal";
 background.setAttributeNode(bclass);
 lordofchaos.setAttributeNode(src);
 cursor.setAttributeNode(src2);
+
 lordofchaos.style.height = "2700px";
 lordofchaos.style.width = "2200px";
 
 
 
 function update(event){
-    var x = event.clientX;
-    var y = event.clientY;
+    var x = event.clientX - 80;
+    var y = event.clientY - 50;
 
     cursor.style.top = y + 'px';
     cursor.style.left = x + 'px';
@@ -28,15 +32,18 @@ document.addEventListener("mousemove", update);
 
 
     document.body.onmousedown = function () {
+
         bclass.value = "evil";
         src2.value = "pictures/weird_guy_staring_evil.png";
     }
     document.body.onmouseup = function () {
+
         bclass.value = "normal";
          src2.value = "pictures/guy_staring.PNG";
     }
     document.body.onkeyup = function () {
         bclass.value = "normal";
+
         src.value = "pictures/weird_guy_staring.png";
         lordofchaos.style.height = "2700px";
         lordofchaos.style.width = "2200px";
@@ -44,6 +51,7 @@ document.addEventListener("mousemove", update);
     }
 
     document.body.onkeydown = function (e) {
+
         if (e.keyCode === 27) {
             bclass.value = "youcant";
         }
@@ -55,5 +63,13 @@ document.addEventListener("mousemove", update);
         }
         if (e.keyCode === 68) {
             bclass.value = "me";
+        }
+        if (e.keyCode === 83) {
+
+            src.value = "pictures/sponge.png"
+        }
+        if (e.keyCode === 88) {
+
+            src.value = "pictures/mr-stark-i-dont-fee-so-good.png";
         }
     }
